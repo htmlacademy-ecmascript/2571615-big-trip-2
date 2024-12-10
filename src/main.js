e9dcd1f49a317bf87488980c470fc514b909c683
Igor Kondratyev <32reggion@mail.ru>
2024-12-10 17:23:59 +0300

    Создаёт моковые объекты

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import BoardPresenter from '../src/presenter/board-presenter';
+import Model from './model/model';
 
 const mainContainer = document.querySelector('.trip-events');
+const model = new Model();
+model.init();
 
-new BoardPresenter(mainContainer).init();
+new BoardPresenter(mainContainer, model).init();
